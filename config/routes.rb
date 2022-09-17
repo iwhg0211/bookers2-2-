13081c5f8067ddbc9cd727c3689bfecf9d577f0d
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-09-17 02:57:10 +0000

    [Add]ログイン、ログアウト別のヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
 end
